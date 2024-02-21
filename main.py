5ae3b65c127cb8112a9b77038db794188e13b82d
Chooook <egr311@ya.ru>
2024-02-21 01:12:18 +0000

    feat!: add possibility to color first only or all matches of phrase refactor and simplify logics remove namedtuple types

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-from collections import namedtuple
 from copy import deepcopy
 
 from docx.text.paragraph import Paragraph, Run
 from docx.shared import RGBColor
 from docx import Document
-
-MyRun = namedtuple('MyRun', ['text', 'style'])
-NewOrigRun = namedtuple('NewOrigRun', ['new', 'original'])
 
 
 class ColorPalette:
@@ -27,82 +23,76 @@ class DocxPainter:
     def __init__(self, document: Document):
         self.__d = document
         self.clr = ColorPalette
-        self.__paragraphs = set(
-            [el for el in self.__d.elements if isinstance(el, Paragraph)])
+        self.__paragraphs = [
+            el for el in self.__d.elements if isinstance(el, Paragraph)]
 
     @property
-    def paragraphs(self) -> set[Paragraph]:
+    def paragraphs(self) -> list[Paragraph]:
         return self.__paragraphs
 
-    def color_list_of_phrases(self, phrases: list[str]):
+    def color_list_of_phrases(
+            self, phrases: list[str], first_only: bool = False):
         for phrase in phrases:
-            self.color_phrase(phrase)
+            self.color_phrase(phrase, first_only)
 
-    def color_phrase(self, phrase: str):
+    def color_phrase(self, phrase: str, first_only: bool = False):
         phrase = phrase.strip()
-        p = self.__get_paragraph_with_phrase(phrase)
-        try:
-            run = self.__find_run_with_phrase(p, phrase, strict=True)
-            self.__color_run(run)
-            return
-        except ValueError:
-            pass
-        try:
-            run = self.__find_run_with_phrase(p, phrase, strict=False)
-            run = self.__reshape_run_with_phrase(p, run, phrase)
-            self.__color_run(run)
-        except ValueError:
-            pass
-        runs = self.__find_runs_with_phrase(p, phrase)
-        # TODO иначе искать группу run`ов
-
-    def __get_paragraph_with_phrase(self, phrase: str) -> Paragraph:
-        for p in self.__paragraphs:
-            if phrase in p.text:
-                return p
-        err_text = 'В тексте нет искомой фразы'
-        raise ValueError(err_text)
+        for p in self.paragraphs:
+            if not self.__find_phrase(p, phrase, strict=False):
+                continue
+            for r in p.runs:
+                if self.__find_phrase(r, phrase, strict=True):
+                    self.__color_r(r)
+                    if first_only:
+                        return
+                    continue
+                if self.__find_phrase(r, phrase, strict=False):
+                    run = self.__reshape_r_with_phrase(p, r, phrase)
+                    self.__color_r(run)
+                    if first_only:
+                        return
+                    continue
+                # runs = self.__find_runs_with_phrase(p, phrase)
+            # FIXME собьётся итерация если сделать reshape? мб нет потому
+            #  что количество увеличивается
+            # TODO иначе искать группу run`ов
 
     @staticmethod
-    def __find_run_with_phrase(p: Paragraph, phrase: str, strict: bool = True):
+    def __find_phrase(
+            el: Run or Paragraph, phrase: str, strict: bool = True) -> bool:
         if strict:
-            for r in p.runs:
-                if phrase == r.text.strip():
-                    return r
-            err_text = 'В параграфе нет объекта run, соответствующего фразе'
-            raise ValueError(err_text)
+            if phrase == el.text.strip():
+                return True
         else:
-            for r in p.runs:
-                if phrase in r.text:
-                    return r
-            err_text = 'В параграфе нет объекта run, содержащего фразу'
-            raise ValueError(err_text)
+            if phrase in el.text:
+                return True
+        return False
 
-    def __color_run(self, run: Run):
-        run.font.color.rgb = self.clr.red
+    def __color_r(self, r: Run):
+        r.font.color.rgb = self.clr.red
 
-    def __reshape_run_with_phrase(self, p: Paragraph, run: Run, phrase: str):
+    def __reshape_r_with_phrase(self, p: Paragraph, r: Run, phrase: str):
         # TODO попробовать выделить отсюда часть по сборке параграфа
-        run_with_phrase_after_split_index = 1
-        run_index = [r.text for r in p.runs].index(run.text)
-        runs_before_phrase = p.runs[:run_index]
-        new_runs = self.__split_run(run, phrase)
-        runs_after_phrase = p.runs[run_index+1:]
-        run_with_phrase = new_runs[run_with_phrase_after_split_index]
+        r_with_phrase_after_split_index = 1
+        r_index = [r.text for r in p.runs].index(r.text)
+        runs_before_phrase = p.runs[:r_index]
+        new_runs = self.__split_r(r, phrase)
+        runs_after_phrase = p.runs[r_index+1:]
+        r_with_phrase = new_runs[r_with_phrase_after_split_index]
         runs = runs_before_phrase + new_runs + runs_after_phrase
         p.clear()
         self.__add_runs(p, runs)
-        return run_with_phrase
+        return r_with_phrase
 
-    def __split_run(self, run: Run, phrase: str):
-        text_parts = run.text.split(phrase, maxsplit=1)
-        first_run = deepcopy(run)
-        first_run.text = text_parts[self.first_el]
-        second_run = deepcopy(run)
-        second_run.text = phrase
-        third_run = deepcopy(run)
-        third_run.text = text_parts[self.last_el]
-        return [first_run, second_run, third_run]
+    def __split_r(self, r: Run, phrase: str):
+        text_parts = r.text.split(phrase, maxsplit=1)
+        first_r = deepcopy(r)
+        first_r.text = text_parts[self.first_el]
+        second_r = deepcopy(r)
+        second_r.text = phrase
+        third_r = deepcopy(r)
+        third_r.text = text_parts[self.last_el]
+        return [first_r, second_r, third_r]
 
     @staticmethod
     def __add_runs(p: Paragraph, runs: list[Run]):
@@ -123,24 +113,14 @@ if __name__ == '__main__':
     #  * `string` целиком в `run`, есть слово после          +
     #  * `string` целиком в `run`, есть слова до и после     +
     #  * `string` разбит на несколько `run`                  -
+    #  * покраска таблиц в .docx                             -
+    #  * покраска всех вхождений одного слова                +
     #  ------------------------------------------------------------------------
 
-    # expected = 'ФРАЗА ЦЕЛИКОМ'
-    # expected = 'ФРАЗА СО СЛОВАМИ ДО'
-    # expected = 'ФРАЗА СО СЛОВАМИ ПОСЛЕ'
-    expected = 'ФРАЗА ДО И ПОСЛЕ'
+    expected = 'СЛОВО'
     # expected = 'СЛОВИЩЕ'
-
-    def debug(ptr):
-        from pprint import pprint
-        for par in ptr.paragraphs:
-            # if string not in p.text:
-            #     continue
-            pprint([r.text for r in par.runs])
 
     d = Document('template.docx')
     painter = DocxPainter(d)
-    # debug(painter)
     painter.color_phrase(expected)
-    # debug(painter)
     d.save('new.docx')
