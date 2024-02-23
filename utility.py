8e2e57f6972a8112b7a29ad7a695dbde20ee088c
Chooook <egr311@ya.ru>
2024-02-23 13:12:09 +0000

    feat: extend color palette

--- utility.py
+++ utility.py
@@ -5,12 +5,32 @@ from docx.shared import RGBColor
 
 @dataclass
 class Color:
-    red = RGBColor(255, 0, 0)
-    orange = RGBColor(255, 255, 0)
-    yellow = RGBColor(0, 255, 255)
-    green = RGBColor(0, 255, 0)
-    blue = RGBColor(0, 0, 255)
-    purple = RGBColor(255, 0, 255)
+    # Basic:
+    red     = RGBColor(255, 0, 0)
+    maroon  = RGBColor(128, 0, 0)
+    yellow  = RGBColor(255, 255, 0)
+    olive   = RGBColor(128, 128, 0)
+    lime    = RGBColor(0, 255, 0)
+    green   = RGBColor(0, 128, 0)
+    aqua    = RGBColor(0, 255, 255)
+    teal    = RGBColor(0, 128, 128)
+    blue    = RGBColor(0, 0, 255)
+    navy    = RGBColor(0, 0, 128)
+    fuchsia = RGBColor(255, 0, 255)
+    purple  = RGBColor(128, 0, 128)
+    black   = RGBColor(0, 0, 0)
+    gray    = RGBColor(128, 128, 128)
+    white   = RGBColor(255, 255, 255)
+    # Alt:
+    darkblue = navy
+    magenta  = fuchsia
+    cyan     = aqua
+    # Extended:
+    orange     = RGBColor(255, 165, 0)
+    pink       = RGBColor(255, 20, 147)
+    coral      = RGBColor(240, 128, 128)
+    violet     = RGBColor(138, 43, 226)
+    aquamarine = RGBColor(127, 255, 212)
 
     def __getitem__(self, item: str) -> RGBColor:
         try:
