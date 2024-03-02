a8d9d806550f8e6829d4daba2970c07ae72f8f19
Chooook <egr311@ya.ru>
2024-03-02 18:07:54 +0000

    feat: add get_paragraphs_with_text func to init

--- docx_styler/__init__.py
+++ docx_styler/__init__.py
@@ -12,11 +12,13 @@ TODO:
 from .change import add_runs, allocate_run_with_text
 from .color import color_run
 from .main import color_text
-from .search import check_text_in_element, get_runs_with_text
+from .search import (check_text_in_element,
+                     get_paragraphs_with_text,
+                     get_runs_with_text)
 
 __all__ = (
     'add_runs', 'allocate_run_with_text',
     'color_run',
     'color_text',
-    'check_text_in_element', 'get_runs_with_text',
+    'check_text_in_element', 'get_paragraphs_with_text', 'get_runs_with_text',
 )
