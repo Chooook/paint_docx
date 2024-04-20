1383e1bdb8aef59186954c611e465e1f3391b41f
Chooook <egr311@ya.ru>
2024-04-20 12:34:45 +0000

    feat!: add fill text funcs
    add class with colors for fill text

--- docx_styler/core/color.py
+++ docx_styler/core/color.py
@@ -1,5 +1,11 @@
 """Модуль с функциями для покраски элементов объекта Document."""
-
+# TODO сделать одну общую сущность, от которой можно брать цвет по типу?
+#  Например, Color.get('red').fill_ms или Color.get('red').rgb_ms
+#  или Color.get('red') -> RGBColor, Color.get('red', 'fill') -> FillColor
+#  ну или Color.get('red', fill=True) -> FillColor, иначе RGBColor
+#  Или наоборот сделать так, чтобы функция сама брала из кортежа нужный тип:
+#  сейчас так и происходит, классы берутся разные, а так интерфейс будет единым
+#  Также можно сделать конструктор RGBColor из кортежа чисел от пользователя
 from docx.shared import RGBColor
 from docx.text.run import Run
 
@@ -10,54 +16,127 @@ def color_run(run: Run, color: str) -> None:
     :param run: Run, который нужно покрасить.
     :param color: Цвет, в который нужно покрасить
     """
-    run.font.color.rgb = Color.get(color)
+    run.font.color.rgb = RgbColor.get(color)
 
 
-class Color:
-    """Класс, содержащий базовые цвета.
+def fill_run_with_color(run: Run, color: str) -> None:
+    """Функция для покраски текста объекта Run.
 
-    Позволяет использовать цвета таким образом:
-    >>> Color.get('color_name')
+    :param run: Run, который нужно покрасить.
+    :param color: Цвет, в который нужно покрасить
+    """
+    run.font.highlight_color = FillColor.get(color)
+
+
+# TODO выделить каждый тип цвета в отдельный модуль?
+class RgbColor:
+    """Класс, содержащий цвета для покраски текста.
+
+    Позволяет извлекать цвета таким образом:
+    >>> RgbColor.get('color_name')
     В случае отсутствия цвета, соответствующего переданному имени
     возвращает красный цвет
     """
 
     # Basic:
     red = RGBColor(255, 0, 0)
-    maroon = RGBColor(128, 0, 0)
+    darkred = RGBColor(128, 0, 0)
     yellow = RGBColor(255, 255, 0)
-    olive = RGBColor(128, 128, 0)
+    darkyellow = RGBColor(128, 128, 0)
     lime = RGBColor(0, 255, 0)
-    green = RGBColor(0, 128, 0)
+    darkgreen = RGBColor(0, 128, 0)
     aqua = RGBColor(0, 255, 255)
     teal = RGBColor(0, 128, 128)
     blue = RGBColor(0, 0, 255)
     navy = RGBColor(0, 0, 128)
-    fuchsia = RGBColor(255, 0, 255)
-    purple = RGBColor(128, 0, 128)
+    magenta = RGBColor(255, 0, 255)
+    violet = RGBColor(128, 0, 128)
     black = RGBColor(0, 0, 0)
     gray = RGBColor(128, 128, 128)
+    lightgray = RGBColor(192, 192, 192)
     white = RGBColor(255, 255, 255)
     # Alt:
+    maroon = darkred
+    olive = darkyellow
+    brightgreen = green = lime
+    turquoise = cyan = aqua
+    darkteal = darkcyan = teal
     darkblue = navy
-    magenta = fuchsia
-    cyan = aqua
+    pink = fuchsia = purple = magenta
+    darkmagenta = darkpurple = violet
+    darkgray = gray50 = gray
+    gray25 = lightgray
     # Extended:
-    orange = RGBColor(255, 165, 0)
-    pink = RGBColor(255, 20, 147)
-    coral = RGBColor(240, 128, 128)
-    violet = RGBColor(138, 43, 226)
-    aquamarine = RGBColor(127, 255, 212)
+    # orange = RGBColor(255, 165, 0)
+    # pink = RGBColor(255, 20, 147)
+    # coral = RGBColor(240, 128, 128)
+    # violet = RGBColor(138, 43, 226)
+    # aquamarine = RGBColor(127, 255, 212)
 
     @classmethod
     def __getitem__(cls, item: str) -> RGBColor:
         """Метод для извлечения цвета по его наименованию."""
         try:
-            return getattr(cls, item)
+            item = ''.join(filter(str.isalpha, item))  # только буквы
+            return getattr(cls, item.lower())
         except AttributeError:
             return cls.red
 
     @classmethod
-    def get(cls, item):
+    def get(cls, item: str):
+        """Метод для доступа к __getitem__ без инициализации объекта."""
+        return cls.__getitem__(item)
+
+
+# TODO выделить каждый тип цвета в отдельный модуль?
+class FillColor:
+    """Класс, содержащий цвета для заливки текста.
+
+    Позволяет извлекать цвета таким образом:
+    >>> FillColor.get('color_name')
+    В случае отсутствия цвета, соответствующего переданному имени
+    возвращает красный цвет
+    """
+    # Basic:
+    red = 6
+    darkred = 13
+    yellow = 7
+    darkyellow = 14
+    lime = 4
+    darkgreen = 11  # GREEN in WD_COLOR_INDEX
+    aqua = 3
+    teal = 10
+    blue = 2
+    navy = 9
+    magenta = 5
+    violet = 12
+    black = 1
+    gray = 15
+    lightgray = 16
+    white = 8
+    auto = 0
+    # Alt:
+    maroon = darkred
+    olive = darkyellow
+    brightgreen = green = lime
+    turquoise = cyan = aqua
+    darkteal = darkcyan = teal
+    darkblue = navy
+    pink = fuchsia = purple = magenta
+    darkmagenta = darkpurple = violet
+    darkgray = gray50 = gray
+    gray25 = lightgray
+
+    @classmethod
+    def __getitem__(cls, item: str):
+        """Метод для извлечения цвета по его наименованию."""
+        try:
+            item = ''.join(filter(str.isalpha, item))  # только буквы
+            return getattr(cls, item.lower())
+        except AttributeError:
+            return cls.red
+
+    @classmethod
+    def get(cls, item: str):
         """Метод для доступа к __getitem__ без инициализации объекта."""
         return cls.__getitem__(item)
--- test/try.py
+++ test/try.py
@@ -1,4 +1,5 @@
 from docx import Document
+# from docx.enum.text import WD_COLOR_INDEX
 
 import docx_styler
 
@@ -7,11 +8,12 @@ if __name__ == '__main__':
     doc = Document('test/template.docx')
 
     docx_styler.color_text(doc, expected, color='red')
+    docx_styler.fill_text_with_color(doc, expected, color='darkgreen')
     # p = docx_styler.get_paragraphs_with_text(
     #     doc, expected, first_only=True)[0]
     # r = docx_styler.get_runs_with_text(
     #     p, expected, first_only=True)[0]
-
+    # r.font.highlight_color = 1
     # # Комментарий к run
     # r.add_comment('Комментарий', author='Полное имя', initials='Инициалы')
     # # run.add_comment ломает документ по какой-то причине
