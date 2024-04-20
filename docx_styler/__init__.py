c3bde6f1909b6e3430607ff5c3fcfe9f72207c08
Chooook <egr311@ya.ru>
2024-04-20 22:21:02 +0000

    refactor: remove add_runs func, change split_run return

--- docx_styler/__init__.py
+++ docx_styler/__init__.py
@@ -3,20 +3,18 @@
 (пока только одна отвёртка и несколько редких бит)
 TODO:
     * Покраска в таблицах
-    * Не красить слово по частям, когда оно в нескольких run,
-        а вытаскивать все его части в один run
     * Добавление комментариев по тексту
     * Расширенная работа со стилями (шрифт, размер, написание, ...)
 """
 
-from .core import add_runs, allocate_run_with_text
+from .core import allocate_run_with_text
 from .core import color_run, fill_run_with_color
 from .main import color_text, fill_text_with_color
 from .core import (check_text_in_element, get_paragraphs_with_text,
                    get_runs_with_text)
 
 __all__ = (
-    'add_runs', 'allocate_run_with_text',
+    'allocate_run_with_text',
     'color_run', 'fill_run_with_color',
     'color_text', 'fill_text_with_color',
     'check_text_in_element', 'get_paragraphs_with_text', 'get_runs_with_text',
