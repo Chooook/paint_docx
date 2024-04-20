6d6b90730206140cd45cd15f3134c73e09b032b0
Chooook <egr311@ya.ru>
2024-04-20 22:21:02 +0000

    refactor: remove add_runs func, change split_run return

--- docx_styler/core/structure.py
+++ docx_styler/core/structure.py
@@ -1,9 +1,12 @@
 """Модуль с функциями, которые изменяют структуру объекта Document."""
 
 from copy import deepcopy
+from typing import Tuple
 
 from docx.text.paragraph import Paragraph
 from docx.text.run import Run
+
+from .static import Index
 
 
 def allocate_run_with_text(paragraph: Paragraph, run: Run, text: str) -> Run:
@@ -11,6 +14,8 @@ def allocate_run_with_text(paragraph: Paragraph, run: Run, text: str) -> Run:
 
     Разделяет исходный Run на 3 Run`а для отделения Run`а с текстом.
     Перезаписывает весь параграф.
+    Метод paragraph.append_runs добавляет Run с пробелом в начало,
+    эта функция очищает Run с пробелом для сохранения структуры параграфа.
     После разделения все три Run`а сохраняют стиль исходного.
     Изменяет исходный объект Document.
 
@@ -19,34 +24,19 @@ def allocate_run_with_text(paragraph: Paragraph, run: Run, text: str) -> Run:
     :param text: Текст, который необходимо выделить в отдельный Run.
     :return: Run, содержащий только необходимый текст.
     """
-    run_with_text_after_split_index = 1
     runs = paragraph.runs
     run_index = [r.text for r in runs].index(run.text)
-    new_runs = __split_run(run, text)
+    new_runs, run_with_text = __split_run(run, text)
     paragraph.clear()
-    add_runs(paragraph, runs[:run_index] + new_runs + runs[run_index + 1:])
-    return new_runs[run_with_text_after_split_index]
+    paragraph.append_runs(runs[:run_index] + new_runs + runs[run_index + 1:])
+    paragraph.runs[Index.first].clear()
+    return run_with_text
 
 
-def add_runs(paragraph: Paragraph, runs: list[Run]) -> None:
-    """Функция для добавления списка Run`ов в Paragraph.
-
-    Исходный метод append_runs добавляет Run с пробелом в начало, эта
-    функция очищает Run с пробелом для сохранения структуры параграфа.
-    Изменяет исходный объект Document.
-
-    :param paragraph: Paragraph, в который нужно добавить Run`ы.
-    :param runs: Run`ы, которые нужно добавить.
-    """
-    runs_number = len(paragraph.runs)
-    paragraph.append_runs(runs)
-    paragraph.runs[runs_number].clear()
-
-
-def __split_run(run: Run, text: str) -> list[Run]:
+def __split_run(run: Run, text: str) -> Tuple[list[Run], Run]:
     first_r = deepcopy(run)
     second_r = deepcopy(run)
     third_r = deepcopy(run)
     first_r.text, third_r.text = run.text.split(text, maxsplit=1)
     second_r.text = text
-    return [first_r, second_r, third_r]
+    return [first_r, second_r, third_r], second_r
