c0da248de7276622b543d183d6f8e95c64f8459f
Chooook <egr311@ya.ru>
2024-09-30 18:43:27 +0400

    refactor: minor

--- docx_styler/core/__init__.py
+++ docx_styler/core/__init__.py
@@ -3,9 +3,11 @@ from .structure import allocate_run_with_text
 from .color import color_run, fill_run_with_color
 from .search import (check_text_in_element, get_paragraphs_with_text,
                      get_runs_with_text)
+from extensions import TextHighlight, SpecifiedTextHighlight
 
 __all__ = (
     'allocate_run_with_text',
     'color_run', 'fill_run_with_color',
     'check_text_in_element', 'get_paragraphs_with_text', 'get_runs_with_text',
+    'TextHighlight', 'SpecifiedTextHighlight'
 )
