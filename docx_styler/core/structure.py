0a439b75c792bddff9f508d7fd0164ac72660d06
Chooook <egr311@ya.ru>
2024-09-30 18:20:19 +0400

    docs: minor update requirements, tasks and docstrings

--- docx_styler/core/structure.py
+++ docx_styler/core/structure.py
@@ -1,4 +1,4 @@
-"""Модуль с функциями, которые изменяют структуру объекта Document."""
+"""Модуль с функциями, для изменения структуры объекта Document."""
 
 from copy import deepcopy
 from typing import Tuple
@@ -12,11 +12,11 @@ from .static import Index
 def allocate_run_with_text(paragraph: Paragraph, run: Run, text: str) -> Run:
     """Функция для выделения объекта run, содержащего необходимый текст.
 
-    Разделяет исходный Run на 3 Run`а для отделения Run`а с текстом.
+    Разделяет исходный run на 3 run`а для отделения run`а с текстом.
     Перезаписывает весь параграф.
     Метод paragraph.append_runs добавляет Run с пробелом в начало,
     эта функция очищает Run с пробелом для сохранения структуры параграфа.
-    После разделения все три Run`а сохраняют стиль исходного.
+    После разделения все три run`а сохраняют стиль исходного.
     Изменяет исходный объект Document.
 
     :param paragraph: Paragraph, содержащий необходимый Run.
@@ -34,6 +34,8 @@ def allocate_run_with_text(paragraph: Paragraph, run: Run, text: str) -> Run:
     paragraph.clear()
     paragraph.append_runs(runs[:run_index] + new_runs + runs[run_index + 1:])
     paragraph.runs[Index.first].clear()
+    # TODO: Реализовать возможность сохранять копию run`а
+    #  вместо изменения на месте
     return run_with_text
 
 
