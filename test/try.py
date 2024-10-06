436e131f9ce7ad7ce33482edfc401762b4df8519
Chooook <egr311@ya.ru>
2024-10-07 00:35:37 +0400

    docs: update tests, correct TODO

--- test/try.py
+++ test/try.py
@@ -1,14 +1,24 @@
+import os
+
 from docx import Document
-# from docx.enum.text import WD_COLOR_INDEX
 
 import docx_styler
 
 if __name__ == '__main__':
-    expected = 'СЛОВО'
-    doc = Document('test/template.docx')
+    filename = 'template.docx'
+    input_dir = 'test'
+    filepath = os.path.join(input_dir, filename)
+    doc = Document(filepath)
 
-    docx_styler.color_text(doc, expected, color='darkred')
-    docx_styler.highlight_text(doc, expected, color='green')
+    expected = 'СЛОВО'
+    docx_styler.color_text(doc, expected, color='green')
+    docx_styler.highlight_text(doc, expected, color='darkred')
+
+    docx_styler.color_text(doc, 'параграф', color=(0, 0, 255))
+    docx_styler.color_text(doc, 'текстом', color=(0, 0, 300))
+    docx_styler.color_text(doc, 'форматированием', color=(0, 0, 255, 14))
+    docx_styler.color_text(doc, 'перед', color=(0, 0))
+    docx_styler.highlight_text(doc, 'Просто', color='dark')
     # p = docx_styler.get_paragraphs_with_text(
     #     doc, expected, first_only=True)[0]
     # r = docx_styler.get_runs_with_text(
@@ -29,4 +39,4 @@ if __name__ == '__main__':
     # # можно брать последний Run в параграфе и менять его стиль.
     # # Создать под это функциональность в core
 
-    doc.save('new.docx')
+    doc.save(f'{filename[:-5]}_result.docx')
