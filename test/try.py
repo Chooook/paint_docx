c14d25ac6b2e4bbd21e0a2c333c35bf67c9b433c
Chooook <egr311@ya.ru>
2025-02-24 00:52:18 +0400

    test: update try.py comments and colors

--- test/try.py
+++ test/try.py
@@ -12,32 +12,15 @@ if __name__ == '__main__':
 
     expected = 'СЛОВО'
     docx_styler.color_text(doc, expected, color='green')
-    docx_styler.highlight_text(doc, expected, color='darkred')
-    docx_styler.color_text(doc, 'ФРАЗА КОТОРУЮ КРАСИМ', color='green')
-    docx_styler.highlight_text(doc, 'ФРАЗА КОТОРУЮ КРАСИМ', color='darkred')
+    docx_styler.highlight_text(doc, expected, color='magenta')
+    docx_styler.color_text(doc, 'ФРАЗА КОТОРУЮ КРАСИМ', color='blue')
+    docx_styler.highlight_text(doc, 'ФРАЗА КОТОРУЮ КРАСИМ', color='yellow')
 
     docx_styler.color_text(doc, 'параграф', color=(0, 0, 255))
+    # Примеры для проверки warnings
     docx_styler.color_text(doc, 'текстом', color=(0, 0, 300))
     docx_styler.color_text(doc, 'форматированием', color=(0, 0, 255, 14))
     docx_styler.color_text(doc, 'перед', color=(0, 0))
     docx_styler.highlight_text(doc, 'Просто', color='dark')
-    # p = docx_styler.get_paragraphs_with_text(
-    #     doc, expected, first_only=True)[0]
-    # r = docx_styler.get_runs_with_text(
-    #     p, expected, first_only=True)[0]
-    # # Комментарий к run
-    # r.add_comment('Комментарий', author='Полное имя', initials='Инициалы')
-    # # run.add_comment ломает документ по какой-то причине
-
-    # # Комментарий к paragraph
-    # p.add_comment('Комментарий', author='Полное имя', initials='Инициалы')
-    # # p.add_comment работает нормально
-
-    # # Примечание к paragraph
-    # p.add_footnote('Примечание')
-    # # add_footnote добавляет порядковый номер примечания в конец параграфа в
-    # # обычном регистре, выглядит дерьмово
-    # # можно брать последний Run в параграфе и менять его стиль.
-    # # Создать под это функциональность в core
 
     doc.save(f'{filename[:-5]}_result.docx')
